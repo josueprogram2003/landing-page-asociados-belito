26bb01ed2f4637d09615f727a8f93966c7dd6fcc
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2024-03-06 15:23:14 -0500

    123

--- src/components/mision-vision/Body.jsx
+++ src/components/mision-vision/Body.jsx
@@ -2,12 +2,12 @@ const Body = () => {
   return (
     <section className="row flex-column align-items-center m-0 py-5" data-aos="zoom-in-up">
       <header className="col-12 col-md-6 my-5 d-flex align-items-center justify-content-around" style={{position:"relative"}}>
-        <div className="container-iconmision">
+        <div className="container-iconmision mx-2">
           <svg width="40" height="40" viewBox="0 0 58 70" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M29 0.166626L0.5 12.8333V31.8333C0.5 49.4083 12.66 65.8433 29 69.8333C45.34 65.8433 57.5 49.4083 57.5 31.8333V12.8333L29 0.166626ZM22.6667 50.8333L10 38.1666L14.465 33.7016L22.6667 41.8716L43.535 21.0033L48 25.5L22.6667 50.8333Z" fill="#2C3941"/>
           </svg>
         </div>
-        <h3 className="text-uppercase fs-2 fw-bold color-primary text-center title-body ms-5 ms-md-0">¿ Por qué elegirnos.? </h3>
+        <h3 className="text-uppercase fs-2 fw-bold color-primary text-center title-body  ms-md-0">¿ Por qué elegirnos.? </h3>
       </header>
       <main className="col-10 col-md-6 py-5 px-lg-5 px-4 mb-5" style={{borderLeft:"4px solid #98722E"}}>
         <p className="color-primary text-body-main">
